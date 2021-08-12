4c120f793e7cca5b0f089249f634ea2dea9fdf98
wangkun <wangkun@baiwang.com>
2021-08-12 22:43:18 +0800

    add promise

--- moli.js
+++ moli.js
@@ -1,10 +1,27 @@
-(function(_) {
-    function k2x() {}
-    k2x.prototype.init = function(options) {
-        const opts = Object.assign({}, options)
-        this.opts = { 
-            ...opts,
-            ok() {}
+class MyPromise {
+    constructor(fn) {
+        this.state = 'pending'
+        this.value = this.reason = undefined
+        const resolve = (value) => {
+            this.state = 'fulfilled'
+            this.value = value
+        }
+        const reject = (reason) => {
+            this.state = 'rejected'
+            this.reason = reason
+        }
+        try {
+            fn(resolve, reject)
+        } catch(e) {
+            reject(new TypeError('构造函数参数不是一个function'))
         }
     }
-})(window)
+    then(onFulfilled, onRejected) {
+        if (this.state === 'fulfilled') {
+            onFulfilled(this.value)
+        }
+        if (this.state === 'rejected') {
+            onRejected(this.reason)
+        }
+    }
+} 
